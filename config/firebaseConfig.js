af44b0bf861c4f692501ade9a07a813bd39ff412
Jesse <jesse.mashoana@gmail.com>
2025-02-03 14:59:50 +0200

    host1

--- config/firebaseConfig.js
+++ config/firebaseConfig.js
@@ -2,22 +2,28 @@ const admin = require('firebase-admin');
 require('dotenv').config();
 
 try {
+  // Construct service account object from environment variables
   const serviceAccount = {
     type: "service_account",
-    project_id: "employee-app-node-cbb49",
-    private_key_id: "dd91645bbb04bf1442fa482772851c714447b9df",
-    private_key: "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCet/vHXd8eMlfb\n...-----END PRIVATE KEY-----\n",
-    client_email: "firebase-adminsdk-7oes7@employee-app-node-cbb49.iam.gserviceaccount.com",
-    client_id: "109203130044663671037",
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY ? JSON.parse(process.env.FIREBASE_PRIVATE_KEY) : undefined,
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_CLIENT_ID,
     auth_uri: "https://accounts.google.com/o/oauth2/auth",
     token_uri: "https://oauth2.googleapis.com/token",
     auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-    client_x509_cert_url: "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-7oes7%40employee-app-node-cbb49.iam.gserviceaccount.com "
+    client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL
   };
+
+  // Validate required credentials
+  if (!serviceAccount.private_key || !serviceAccount.project_id || !serviceAccount.client_email) {
+    throw new Error('Missing required Firebase credentials');
+  }
 
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
-    storageBucket: "employee-app-node-cbb49.appspot.com"
+    storageBucket: `${serviceAccount.project_id}.appspot.com`
   });
 
   const db = admin.firestore();
